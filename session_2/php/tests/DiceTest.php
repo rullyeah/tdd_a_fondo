18b6ec1f87c782def98804d21091f45c4e3b3cbd
rnadal <rullyeah@gmail.com>
2017-11-26 00:23:23 +0100

    Tests: Roll should be less than or equal the maximum

--- session_2/php/tests/DiceTest.php
+++ session_2/php/tests/DiceTest.php
@@ -10,4 +10,10 @@ class DiceTest extends TestCase
         $dice = new Dice;
         $this->assertGreaterThanOrEqual(Dice::MIN_ROLL, $dice->roll());
     }
+
+    public function testDiceShouldReturnANumberLessThanOrEqualTheMaximum()
+    {
+        $dice = new Dice;
+        $this->assertLessThanOrEqual(6, $dice->roll());
+    }
 }
